3a12e7e34825e3b513fd4d60f6cbf184572450eb
Eric Wang <ejwang@stanford.edu>
2019-02-17 06:33:54 -0800

    format jordan's blurb

--- src/AppSidebar.tsx
+++ src/AppSidebar.tsx
@@ -71,40 +71,42 @@ export default class AppSidebar extends React.Component<AppSidebarProps> {
             }
             </div>
             <p>
-              <strong>A Calamari Comitatus production</strong>
-            </p>
-            <p>
               Imagine that one day you're watching the news with your friend and this
               politician who you don't know pops up on the TV. "Who's Kirsten Gillibrand?"
               you ask your friend. "What does she believe?"
-
+            </p>
+            <p>
               "Oh, well, she's sorta like Elizabeth Warren," your friend might answer.
               Now, while this is obviously oversimplifying things, there's some truth here.
               Kirsten Gillibrand is definitely more like Elizabeth Warren then like Ted Cruz.
               But how would you even begin to compare the two?
-
+            </p>
+            <p>
               For starters, you could look at voting records. See what percentage of policy
               issues these two politicians agree on. But what if what you want to go further
-              than individual politicians and understand groups of politicians? And maybe even
-              identify clusters and voting blocks between them.
-
-              Well, you could use this neat machine learning algorithm called
-              principal components analysis (PCA). Here, we use PCA to find the two
+              than individual politicians and understand groups of politicians? Or maybe even
+              identify clusters and voting blocks between them?
+            </p>
+            <p>
+              You could use this neat machine learning algorithm called
+              principal component analysis (PCA). Here, we use PCA to find the two
               most significant variables that influence how senators vote.  We can then
               use this information to visualize naturally arising groups of polticians
               with similar behaviors and beliefs. 
-
-              The most obvious cluster is political party--no matter the issue, our senators
+            </p>
+            <p>
+              The most obvious cluster is political party: no matter the issue, our senators
               appear to be deeply partisan. But if you look closer, you'll find a few other
               interesting clusters, like a small group of democratic-socialist-leaning
               politicians around Bernie Sanders. 
-
-              So go ahead, play around and see if you can find a few other interesting trends! 
+            </p>
+            <p>
+              Play around and see if you can find a few other interesting trends! 
              </p>
+             <p>
+              <strong>Copyright 2019 Calamari Comitatus</strong>
+            </p>
          </div>
-        </div>
-        <div className={Classes.DRAWER_FOOTER}>
-          <Button text="Close" onClick={this.props.closeSidebar} />
         </div>
       </>
     );
