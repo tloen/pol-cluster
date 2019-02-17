8d6d9b473253f60276f39292bdb9be7814807be5
Eric Wang <ejwang@stanford.edu>
2019-02-16 23:29:27 -0800

    pull and parse and PCA

--- src/types.ts
+++ src/types.ts
@@ -1,1 +1,10 @@
-export interface VisualizationOptions { }
+export interface VisualizationOptions {}
+
+export type PointData = [number, number];
+
+export type DisplayData = PointData[];
+
+export interface DisplayInfo {
+  options: VisualizationOptions;
+  data: DisplayData;
+}
