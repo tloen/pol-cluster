2c37632d13be355fb90365be98aedfef660485c6
Eric Wang <ejwang@stanford.edu>
2019-02-17 06:50:47 -0800

    blurb

--- src/AppSidebar.tsx
+++ src/AppSidebar.tsx
@@ -71,38 +71,32 @@ export default class AppSidebar extends React.Component<AppSidebarProps> {
             }
             </div>
             <p>
-              Imagine that one day you're watching the news with your friend and this
-              politician who you don't know pops up on the TV. "Who's Kirsten Gillibrand?"
-              you ask your friend. "What does she believe?"
+              Imagine you're watching the news with a friend when a
+              politician you don't know pops up on the TV. "Who's Kirsten Gillibrand?"
+              you ask. "What does she believe?"
             </p>
             <p>
-              "Oh, well, she's sorta like Elizabeth Warren," your friend might answer.
-              Now, while this is obviously oversimplifying things, there's some truth here.
-              Kirsten Gillibrand is definitely more like Elizabeth Warren then like Ted Cruz.
-              But how would you even begin to compare the two?
+              "Well, she's sorta like Elizabeth Warren," your friend might answer.
+              While this is obviously oversimplifying things,          Kirsten Gillibrand is definitely more like Elizabeth Warren then like Ted Cruz.
+              But how would you actually demonstrate this?
             </p>
             <p>
-              For starters, you could look at voting records. See what percentage of policy
-              issues these two politicians agree on. But what if what you want to go further
-              than individual politicians and understand groups of politicians? Or maybe even
-              identify clusters and voting blocks between them?
-            </p>
-            <p>
-              You could use this neat machine learning algorithm called
-              principal component analysis (PCA). Here, we use PCA to find the two
+              You could use a machine learning algorithm called
+              <strong>principal component analysis (PCA)</strong>. Here, we use PCA to find the two
               most significant variables that influence how senators vote.  We can then
               use this information to visualize naturally arising groups of polticians
               with similar behaviors and beliefs. 
             </p>
             <p>
-              The most obvious cluster is political party: no matter the issue, our senators
+              The most obvious division is political party: no matter the issue, our senators
               appear to be deeply partisan. But if you look closer, you'll find a few other
               interesting clusters, like a small group of democratic-socialist-leaning
               politicians around Bernie Sanders. 
             </p>
             <p>
-              Play around and see if you can find a few other interesting trends! 
+              Play around a bit and see if you can find a few other interesting trends! 
              </p>
+             <br />
              <p>
               <strong>Copyright 2019 Calamari Comitatus</strong>
             </p>
